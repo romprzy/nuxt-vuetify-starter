9091d154c5a1738f0746596fc5aa1a81f4b1352c
Roman Przybylski <romprzy81@gmail.com>
2024-09-25 16:39:46 +0200

    Base meta

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
 <template>
   <NuxtLayout />
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Nuxt vuetify starter',
+  meta: [
+    {
+      name: 'description',
+      content: 'Starter pack for nuxt development with Vuetify.',
+    },
+  ],
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   build: {
